d68eec4fd3fd37293a73b8dfd457fc199be0d585
Arthi Govindasamy <arthigovindasamyga.official@gmail.com>
2026-02-27 21:48:38 +0530

    Update G Recursion.java

--- G Recursion.java
+++ G Recursion.java
@@ -101,6 +101,33 @@ static int sumOfDigits(int n){
 3.....sum of array
 4.....check sorted array or not
 5.....print divisors of number
+    class DivisorsRecursion {
+
+    static void printDivisors(int n, int i) {
+
+       
+        if (i > Math.sqrt(n)) {
+            return;
+        }
+
+        if (n % i == 0) {
+
+            System.out.print(i + " ");
+
+            if (i != n / i) {
+                System.out.print(n / i + " ");
+            }
+        }
+
+        
+        printDivisors(n, i + 1);
+    }
+
+    public static void main(String[] args) {
+        int n = 12;
+        printDivisors(n, 1);
+    }
+}
 
 6...POWER OF A NUMBER
 class PowerOfN {
